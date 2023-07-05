91b01ed204b0c266ec5ce41a5b52f4d70d879b41
unknown <luqita.dmayo@gmail.com>
2023-07-05 17:20:56 -0300

    borro el prompt

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-let respuesta = prompt("¿Porque la luna es mas grande que el sol?","porque sale de noche").toLowerCase()
+let respuesta = prompt("¿Porque la luna es mas grande que el sol?").toLowerCase()
 let intentos=1
 while (respuesta !== "porque sale de noche") {
     alert("Intenta de nuevo")
     intentos++
-    respuesta = prompt("¿Porque la luna es mas grande que el sol?","porque sale de noche").toLowerCase()
+    respuesta = prompt("¿Porque la luna es mas grande que el sol?").toLowerCase()
 }
 alert("correcto! Lo hiciste en "+intentos+" intentos")
